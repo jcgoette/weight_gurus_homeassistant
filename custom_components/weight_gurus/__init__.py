3680b95c67e763a0fee7b56c24b1d1b62930ef84
jcgoette <jcgoette@gmail.com>
2021-06-18 22:43:41 -0400

    add support for unloading entries

--- custom_components/weight_gurus/__init__.py
+++ custom_components/weight_gurus/__init__.py
@@ -10,4 +10,12 @@ async def async_setup_entry(hass, entry):
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
+
+    return True
+
+
+async def async_unload_entry(hass, entry):
+    """Unload Weight Gurus entity."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+
     return True
