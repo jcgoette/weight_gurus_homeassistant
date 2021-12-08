97a0e0ee1d3fad5346fbf18d7f3509469ecb0473
jcgoette <jcgoette@gmail.com>
2021-12-07 22:07:23 -0500

    housekeeping

--- custom_components/weight_gurus/config_flow.py
+++ custom_components/weight_gurus/config_flow.py
@@ -31,10 +31,6 @@ class WeightGurusFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle a flow initiated by the user."""
         self._errors = {}
 
-        # Uncomment the next 2 lines if only a single instance of the integration is allowed:
-        # if self._async_current_entries():
-        #     return self.async_abort(reason="single_instance_allowed")
-
         if user_input is not None:
             valid = await self._test_credentials(
                 user_input[CONF_EMAIL], user_input[CONF_PASSWORD]
